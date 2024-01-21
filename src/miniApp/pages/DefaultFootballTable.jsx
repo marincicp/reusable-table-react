721201184ecdd26acd4ddccfc047b66ac2264de0
marincicp <marincicpetar@gmail.com>
2024-01-21 21:37:28 +0100

    changes

--- src/miniApp/pages/DefaultFootballTable.jsx
+++ src/miniApp/pages/DefaultFootballTable.jsx
@@ -6,17 +6,7 @@ function DefaultFootballTable() {
   return (
     <div className="center">
       <Table columns="5rem minmax(min-content, 1fr) repeat(4,5rem) 7rem 5rem">
-        <Table.Header>
-          <th>#</th>
-          <th>Club</th>
-          <th>PM</th>
-          <th>W</th>
-          <th>D</th>
-          <th>L</th>
-          <th>GD</th>
-          <th>P</th>
-        </Table.Header>
-
+        <Table.Header header="# Club PM W D L GD P" />
         <Table.Body data={footballTableData}></Table.Body>
       </Table>
     </div>
